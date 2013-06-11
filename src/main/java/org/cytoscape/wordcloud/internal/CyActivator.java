2a3f3a42eb9f17a19bafb9fb4c227cbc5f4038e3
Yue <oryato@gmail.com>
2013-06-11 18:20:09 -0400

    Attempt to get the corresponding row for a node

--- src/main/java/org/cytoscape/wordcloud/internal/CyActivator.java
+++ src/main/java/org/cytoscape/wordcloud/internal/CyActivator.java
@@ -1,5 +1,6 @@
 package org.cytoscape.wordcloud.internal;
 
+import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.application.swing.CySwingApplication;
 
 
@@ -10,6 +11,11 @@ import org.cytoscape.event.CyListener;
 import org.osgi.framework.BundleContext;
 
 import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNetworkManager;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
+import org.cytoscape.model.CyTable;
+import org.cytoscape.model.CyTableManager;
 import org.cytoscape.model.CyTableUtil;
 import org.cytoscape.model.events.RowSetRecord;
 import org.cytoscape.model.events.RowsSetEvent;
@@ -19,6 +25,7 @@ import org.cytoscape.wordcloud.internal.MyCytoPanel;
 import org.cytoscape.wordcloud.internal.Sample28;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Properties;
 
 
@@ -43,7 +50,11 @@ public class CyActivator extends AbstractCyActivator {
 		ShowWordCloudAction showWordCloudAction = new ShowWordCloudAction();
 		
 		registerService(bc, showWordCloudAction, CyAction.class, new Properties());
-		CyListener a;
+		
+		CyApplicationManager cyApplicationManager = getService(bc, CyApplicationManager.class);
+		
+		WordCloudRowSetListener wordCloudRowSetListener = new WordCloudRowSetListener(cyApplicationManager);
+		registerAllServices(bc, wordCloudRowSetListener, new Properties());
 		
 		RowsSetListener rowsSetListener = new RowsSetListener() {
 			
@@ -53,10 +64,49 @@ public class CyActivator extends AbstractCyActivator {
 				
 				System.out.println("Selection event? " + e.containsColumn("selected"));
 				
-				System.out.println("id Event? " + e.containsColumn("o_id28349278l"));
+//				System.out.println("id Event? " + e.containsColumn("o_id28349278l"));
 				
+				//RowSetRecord a;
+				
+				RowSetRecord a1;
+		
+				
+//				e.getSource();
+		
+				// System.out.println("Source table: " + e.getSource().getSUID());
+				
+				/*
+				List<CyNode> selectedNodes = fetchSelectedNodes(cyApplicationManager.getCurrentNetwork());
+				System.out.println("Number of selected nodes on current network: " + selectedNodes.size());
+				
+				CyTable cyTable = cyApplicationManager.getCurrentTable();
+				*/
+				
+				//cyTable.
+				
+//				cyTable.getRow(primaryKey);
+				
+				/*
+				System.out.println("Start printing all attributes");
+				for (CyRow cyRow : cyTable.getAllRows()) { 
+					System.out.println(cyRow.getAllValues());
+				}
+				*/
+				// CyTable cyTable = e.getSource();
+				
+				
+				/*
+				CyNetwork a;
+				CyApplicationManager c1;
+//				c1.
+				CyNetworkManager c2;
+				//c2.
 				// Collection<RowSetRecord> rowSetRecords = e.getColumnRecords("selected");
-
+				CyTableManager c3;
+				
+				CySwingApplication c4;
+				*/
+				
 				/*
 				System.out.println("Start Print RowSetRecords");
 				for (RowSetRecord rowSetRecord : rowSetRecords) {
@@ -67,12 +117,10 @@ public class CyActivator extends AbstractCyActivator {
 			}
 		};
 		
-		registerAllServices(bc, rowsSetListener, new Properties());
+//		registerAllServices(bc, rowsSetListener, new Properties());
 		System.out.println("rowSetListener registered");
 	}
 	
-	private Collection<CyNode> fetchSelectedNodes(CyNetwork network) {
-		return CyTableUtil.getNodesInState(network,"selected",true);
-	}
+
 }
 
