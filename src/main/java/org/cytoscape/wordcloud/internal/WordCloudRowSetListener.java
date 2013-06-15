fc80150fbe67662bffd5081c92fd1e22c94895b1
Yue Dong <oryato@gmail.com>
2013-06-15 13:17:03 -0400

    Added methods for obtaining the attribute strings for a given set of nodes

--- src/main/java/org/cytoscape/wordcloud/internal/WordCloudRowSetListener.java
+++ src/main/java/org/cytoscape/wordcloud/internal/WordCloudRowSetListener.java
@@ -1,10 +1,17 @@
 package org.cytoscape.wordcloud.internal;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNetworkTableManager;
 import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
 import org.cytoscape.model.CyTable;
 import org.cytoscape.model.CyTableUtil;
 import org.cytoscape.model.events.RowsSetEvent;
@@ -27,19 +34,85 @@ public class WordCloudRowSetListener implements RowsSetListener {
 		System.out.println("Number of selected nodes on current network: " + selectedNodes.size());
 		
 		// Get the table for the current network
-		CyTable cyTable = cyApplicationManager.getCurrentTable(); // e.getSource();
+		CyTable cyTable = e.getSource();
 		
-		CyNode selectedNode = selectedNodes.iterator().next();
-		System.out.println("Selected Node: " + selectedNode);
+		// Notes:
+		// 1. Use CyNetworkTableManager to get network from table
+		// 2. Use cyTable.getRow(cyNode) to get row
 		
-		System.out.println("cyTable.getRow(): " + cyTable);
+		if (selectedNodes != null && selectedNodes.size() > 0) {
 		
-		System.out.println("cyTable.getRow(): " + cyTable.getRow(selectedNode));
-		System.out.println("Row for a given selected node: " + cyTable.getRow(selectedNode).getAllValues());
+			CyNode selectedNode = selectedNodes.iterator().next();
+			System.out.println("Selected Node: " + selectedNode);
+			
+			System.out.println("cyTable: " + cyTable);
+			
+			System.out.println("cyTable.getRow(): " + cyTable.getRow(selectedNode.getSUID()));
+			System.out.println("Row for a given selected node: " + cyTable.getRow(selectedNode.getSUID()).getAllValues());
+			
+			System.out.println("Test1");
+			
+			System.out.println("getWordCounts(): " + this.getWordCounts(selectedNodes, this.cyApplicationManager.getCurrentNetwork()));
+		}
 	}
 	
 	private List<CyNode> fetchSelectedNodes(CyNetwork network) {
 		return CyTableUtil.getNodesInState(network, "selected", true);
+	}
+	
+	// Make sure the right CyTable is given, if multiple tables contain the nodes
+	private Map<String, Integer> getWordCounts(Collection<CyNode> nodes, CyNetwork network) {
+		
+		Map<String, Integer> wordCounts = new HashMap<String, Integer>();
+		
+		for (CyNode node : nodes) {
+			List<String> nodeWords = this.getWords(node, network);
+			
+			for (String word : nodeWords) {
+				if (wordCounts.get(word) == null) {
+					wordCounts.put(word, 1);
+				} else {
+					wordCounts.put(word, wordCounts.get(word) + 1);
+				}
+			}
+		}
+		
+		
+		return wordCounts;
+	}
+	
+
+	/**
+	 * Get the words for a given node
+	 * 
+	 * @param node The node whose attribute words to get
+	 * @param network The network that the node belongs to, used to retrieve attributes
+	 * @return
+	 */
+	private List<String> getWords(CyNode node, CyNetwork network) {
+		List<String> words = new LinkedList<String>();
+		
+		CyRow correspondingRow = network.getRow(node);
+		
+		Map<String, Object> rowValues = correspondingRow.getAllValues();
+		//System.out.println("getWords() call, getAllValues: " + correspondingRow.getAllValues());
+		
+		for (Object value : rowValues.values()) {
+			if (value instanceof String) {
+				words.add((String) value);
+			} else if (value instanceof List) {
+				
+				List<?> list = (List<?>) value;
+				
+				for (Object listObject : list) {
+					if (listObject instanceof String) {
+						words.add((String) listObject);
+					}
+				}
+			}
+		}
+		
+		return words;
 	}
 
 }
