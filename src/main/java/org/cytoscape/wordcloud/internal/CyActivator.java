0a5c5dca7662dfe05f09f120eb8132e493dc44e1
Yue Dong <oryato@gmail.com>
2013-07-04 14:10:42 -0400

    Removed unused imports

--- src/main/java/org/cytoscape/wordcloud/internal/CyActivator.java
+++ src/main/java/org/cytoscape/wordcloud/internal/CyActivator.java
@@ -4,28 +4,10 @@ import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.application.swing.CySwingApplication;
 
 
-import org.cytoscape.application.swing.CytoPanelComponent;
 import org.cytoscape.application.swing.CyAction;
-import org.cytoscape.event.CyListener;
-
 import org.osgi.framework.BundleContext;
 
-import org.cytoscape.model.CyNetwork;
-import org.cytoscape.model.CyNetworkManager;
-import org.cytoscape.model.CyNode;
-import org.cytoscape.model.CyRow;
-import org.cytoscape.model.CyTable;
-import org.cytoscape.model.CyTableManager;
-import org.cytoscape.model.CyTableUtil;
-import org.cytoscape.model.events.RowSetRecord;
-import org.cytoscape.model.events.RowsSetEvent;
-import org.cytoscape.model.events.RowsSetListener;
 import org.cytoscape.service.util.AbstractCyActivator;
-import org.cytoscape.wordcloud.internal.MyCytoPanel;
-import org.cytoscape.wordcloud.internal.Sample28;
-
-import java.util.Collection;
-import java.util.List;
 import java.util.Properties;
 
 
